e9169644faa834ecc86d1cf923b2e59430345140
kvnq-poza <kvnqpoza@gmail.com>
2026-02-21 19:00:14 +0100

    Add config and UI theme styles

---
+++ ogrite-frontend/next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {},
+  turbopack: {
+    root: __dirname,
+  },
+};
+
+export default nextConfig;
